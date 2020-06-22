32ea51dfc85aa9c5d3d4725870424c2fbed3a1cd
kiku ishimoto <kikuish@gmail.com>
2020-06-22 00:03:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,17 +1,15 @@
 
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+  get '/' do
+      erb :user_input
+  end
 
-get '/' do
-  erb :user_input
-end
+  post '/piglatinize' do
+      @result = PigLatinizer.new.piglatinize(params[:user_phrase])
 
-post '/piglatinize' do
-  user_word = params["user_phrase"]
-  word = PigLatinizer.new
-  @pig_latinized_word = word.to_pig_latin(user_word)
-
-  erb :pit_latin
-end
+      erb :piglatinized
+  end
 end
