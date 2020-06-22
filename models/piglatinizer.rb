0ce9e7caca87c3c49912514c7ca30da29dd231c8
kiku ishimoto <kikuish@gmail.com>
2020-06-22 00:03:40 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,13 +3,13 @@ class PigLatinizer
     def piglatinize(input_str)
       input_str.split(" ").map{ |word| piglatinize_word(word)}.join(" ")
     end
-  
+
     private
-  
+
     def vowel?(char)
       char.match(/[aAeEiIoOuU]/)
     end
-  
+
     def piglatinize_word(word)
       # word starts with vowel
       if vowel?(word[0])
@@ -22,5 +22,5 @@ class PigLatinizer
         word_leftover + consonants + "ay"
       end
     end
-  
+
   end
